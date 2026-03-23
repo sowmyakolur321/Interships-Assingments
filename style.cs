73ae3b7ef77b9690eb00a78cefbf2950521fa5c2
sowmyakolur321 <sowmyakolur.ise.rymec@gmail.com>
2026-03-23 18:49:08 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,43 @@
+body {
+    background-color: #e6f2ff;
+    font-family: Arial, sans-serif;
+}
+
+.container {
+    width: 60%;
+    margin: auto;
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px gray;
+    text-align: center;
+}
+
+h1 {
+    color: #0073e6;
+}
+
+h2 {
+    color: #333;
+}
+
+img {
+    width: 150px;
+    border-radius: 50%;
+    border: 3px solid #0073e6;
+}
+
+ul, ol {
+    text-align: left;
+    margin-left: 40%;
+}
+
+a {
+    display: inline-block;
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #0073e6;
+    color: white;
+    text-decoration: none;
+    border-radius: 5px;
+}
